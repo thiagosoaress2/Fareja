28d5692f8847364f4e4227458f6787de3cf73762
Thiago Soares <64110623+thiagosoaress2@users.noreply.github.com>
2020-08-16 21:33:01 -0300

    implementando userClass

---
+++ app/src/main/java/com/petcare/petcare/Utils/userClass.kt
@@ -0,0 +1,46 @@
+package com.petcare.petcare.Utils
+
+import com.facebook.internal.Mutable
+
+class userClass {
+
+    // property (data member)
+    var userMail = "nao"
+    var userBD = "nao"
+    var alvara = "nao"
+    var tipo = "usuario"
+    var petBDseForEmpresario = "nao"
+    var bdDoPet = "nao"
+    var plano = "nao"
+    var imgDoUser = "nao"
+
+    // member function
+    fun getUserInfo() : MutableList<String> {
+
+        val infos: MutableList<String> = ArrayList()
+
+
+        return infos
+    }
+
+
+    // member function
+    fun getProprietarioInfo() : MutableList<String> {
+
+        val infos: MutableList<String> = ArrayList()
+
+
+        return infos
+    }
+
+
+    // member function
+    fun getAutonomoInfo() : MutableList<String> {
+
+        val infos: MutableList<String> = ArrayList()
+
+
+        return infos;
+    }
+
+}
